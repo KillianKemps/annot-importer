539ac95b01376d653b15c5f43c5365852123412a
Killian Kemps <developer@killiankemps.fr>
2018-02-11 15:19:10 +0100

    Install Docopt

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Usage: main.py ANNOTATION_FILE
+
+Convert annotation file to Markdown
+
+Arguments:
+  ANNOTATION_FILE File to import
+
+Options:
+  -h --help
+"""
+
+from docopt import docopt
+import csv
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
